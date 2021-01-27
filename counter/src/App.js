6fa24c0bd783966e518bf81f7a5dc9da132e4dc4
Ewa Kadziolka <kalistaewa@gmail.com>
2021-01-27 14:05:25 +0100

    01

--- counter/src/App.js
+++ counter/src/App.js
@@ -3,23 +3,34 @@ import logo from "./logo.svg";
 import "./App.css";
 
 class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state={
+      count:0
+    }
+  }
+
+  decrementCount = () =>{
+    this.setState({count: this.state.count--});
+    console.log(this.setState.count);
+  }
+
+  incrementCount = () =>{
+    this.setState({count: this.state.count++});
+    console.log(this.setState.count);
+  }
+
+
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+       <h1>Counter</h1>
+       <h2>{this.state.count}</h2>
+       <button onClick={this.incrementCount}>+</button>
+       <button onClick={this.decrementCount}>-</button>
+       
+
       </div>
     );
   }
