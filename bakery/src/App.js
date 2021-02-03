51b5d3a6f6b7d0604e54f7106d89aba6464e1706
Ewa Kadziolka <kalistaewa@gmail.com>
2021-02-03 11:01:45 +0100

    functions select and renderContent

--- bakery/src/App.js
+++ bakery/src/App.js
@@ -11,7 +11,40 @@ class App extends React.Component {
     super(props);
     this.state = {
       activeTab: "add",
+      items: [],
     };
+    this.selectAdd = this.selectAdd.bind(this);
+    this.selectList = this.selectList.bind(this);
+    this.selectPay = this.selectPay.bind(this);
+    this.renderContent = this.renderContent.bind(this);
+  }
+
+  selectAdd(e) {
+    this.setState({
+      activeTab: "add",
+    });
+  }
+  selectList(e) {
+    this.setState({
+      activeTab: "list",
+    });
+  }
+  selectPay(e) {
+    this.setState({
+      activeTab: "pay",
+    });
+  }
+
+  renderContent() {
+    if (this.state.activeTab === "add") {
+      return <Add></Add>;
+    }
+    if (this.state.activeTab === "list") {
+      return <List></List>;
+    }
+    if (this.state.activeTab === "pay") {
+      return <Pay></Pay>;
+    }
   }
 
   render() {
@@ -20,35 +53,38 @@ class App extends React.Component {
         <h1 className="my-3 ">Notre boulangerie</h1>
         <div className="buttons row">
           <Buttons
+            onClick={this.selectAdd}
             isSelected={
               this.state.activeTab === "add"
-                ? "form-control col-2 btn btn-primary"
-                : "form-control col-2 btn btn-outline-secondary"
+                ? "form-control col-2 mx-1 btn btn-primary"
+                : "form-control col-2 mx-1 btn btn-outline-secondary"
             }
           >
-            Add
+            Acheter
           </Buttons>
           <Buttons
+            onClick={this.selectList}
             isSelected={
               this.state.activeTab === "list"
-                ? "form-control col-2 btn btn-primary"
-                : "form-control col-2 btn btn-outline-secondary"
+                ? "form-control col-2 mx-1 btn btn-primary"
+                : "form-control col-2 mx-1 btn btn-outline-secondary"
             }
           >
             List
           </Buttons>
 
           <Buttons
+            onClick={this.selectPay}
             isSelected={
               this.state.activeTab === "pay"
-                ? "form-control col-2 btn btn-primary"
-                : "form-control col-2 btn btn-outline-secondary"
+                ? "form-control col-2 mx-1 btn btn-primary"
+                : "form-control col-2 mx-1 btn btn-outline-secondary"
             }
           >
-            Pay
+            Payement
           </Buttons>
         </div>
-        <Add />
+        {this.renderContent()}
       </div>
     );
   }
