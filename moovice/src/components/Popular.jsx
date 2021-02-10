e1b0486e763d0ba1ba8353d9243c451a04976666
Ewa Kadziolka <kalistaewa@gmail.com>
2021-02-10 17:01:58 +0100

    API works

--- moovice/src/components/Popular.jsx
+++ moovice/src/components/Popular.jsx
@@ -1,10 +1,30 @@
 import React from "react";
 
 class Popular extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      movies: [],
+    };
+  }
+
+  componentDidMount() {
+    let url =
+      "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=e6518e67714e9044605198a5d76685a3";
+    fetch(url)
+      .then((result) => result.json())
+      .then((filmsFromApi) => {
+        console.log("resultat = ", filmsFromApi);
+        this.setState({
+          movies: filmsFromApi,
+        });
+      });
+  }
+
   render() {
     return (
       <main>
-        <h1>Je suis la page Popular</h1>
+        <div className="container"></div>
       </main>
     );
   }
