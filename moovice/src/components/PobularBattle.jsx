8b6a00b382869072d05e4f3d46223820371a95ba
Ewa Kadziolka <kalistaewa@gmail.com>
2021-02-11 14:41:10 +0100

    popularBatte not yet working

--- moovice/src/components/PobularBattle.jsx
+++ moovice/src/components/PobularBattle.jsx
@@ -1,12 +1,45 @@
 import React from "react";
+import Card from "./Card";
 
 class PopularBattle extends React.Component {
-  render() {
+  constructor(props) {
+    super(props);
+    this.state = {
+      movies: [],
+      currentBattle: 0,
+    };
+  }
+
+  componentDidMount() {
+    let url =
+      "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=e6518e67714e9044605198a5d76685a3";
+    fetch(url)
+      .then((result) => result.json())
+      .then((filmsFromApi) => {
+        console.log("resultat = ", filmsFromApi);
+        this.setState({
+          movies: filmsFromApi.results,
+        });
+      });
+  }  
+  }
+
+  render(){
     return (
       <main>
-        <h1>Je suis la page PopularBattle</h1>
+        <div className="container">
+          <div className="row">
+            {this.state.movies.filter((movie)=>{this.state.movies[Math.floor(Math.random() * (this.state.movies.length+1)]}).map((movie, index) => {
+              return <Card filmTitle={movie} key={`film-list-key ${index}`} />;
+            })}
+            {this.state.movies.filter((movie)=>{this.state.movies[Math.floor(Math.random() * (this.state.movies.length+1)]}).map((movie, index) => {
+              return <Card filmTitle={movie} key={`film-list-key ${index}`} />;
+            })}
+          </div>
+        </div>
       </main>
     );
   }
 }
+
 export default PopularBattle;
--- moovice/src/components/Popular.jsx
+++ moovice/src/components/Popular.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Card from "./Card";
+import PopularBattle from "./PobularBattle";
 
 class Popular extends React.Component {
   constructor(props) {
